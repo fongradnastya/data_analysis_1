b84e22c64530aafdd2fd00dd7630c9bda94f2b65
fongradnastya <fongradnastya@gmail.com>
2023-03-03 10:19:34 +0700

    New

--- notebooks/plot_graph.py
+++ notebooks/plot_graph.py
@@ -73,19 +73,18 @@ def build_three_hists(data_f, columns):
     plt.show()
 
 
-def compare_category_graf(data_1, data_2, column_1, column_2, column_name):
+def compare_category_graf(data_1, data_2, column_1, column_2):
     """
     Сопоставляет графики двух категориальных признаков
     :param data_1: первый фрейм данных
     :param data_2: второй фрейм данных
     :param column_1: столбец первого фрейма
     :param column_2: столбец второго фрейма
-    :param column_name: название сопоставляемых
     :return:
     """
     n_data_1 = data_1[column_1].value_counts()
     n_data_2 = data_2[column_2].value_counts()
-    fig = plt.figure(figsize=(15, 10))
+    fig = plt.figure(figsize=(20, 15))
     # Adds subplot on position 1
     ax = fig.add_subplot(221)
     # Adds subplot on position 2
@@ -94,55 +93,17 @@ def compare_category_graf(data_1, data_2, column_1, column_2, column_name):
     ax4 = fig.add_subplot(224)
     ax.pie(n_data_1.values, labels=n_data_1.index)
     ax.legend(bbox_to_anchor=(0.9, 1))
-    ax.set_title(f"Круговая диаграмма {column_name + ' 1'}")
+    ax.set_title(f"Круговая диаграмма {column_1 + ' до изменения'}")
     ax2.bar(x=n_data_1.index, height=n_data_1.values)
-    ax2.set_title(f"Столбчатая диаграмма {column_name + ' 1'}")
-    plt.setp([ax2], xlabel='значения выборки', ylabel='частота')
+    ax2.set_title(f"Столбчатая диаграмма {column_1 + ' до изменения'}")
+    plt.setp([ax2, ax4], xlabel='значения выборки', ylabel='частота')
     ax3.pie(n_data_2.values, labels=n_data_2.index)
     ax3.legend(bbox_to_anchor=(0.9, 1))
-    ax3.set_title(f"Круговая диаграмма {column_name + ' 2'}")
+    ax3.set_title(f"Круговая диаграмма {column_1 + ' после изменения'}")
     ax4.bar(x=n_data_2.index, height=n_data_2.values)
-    ax4.set_title(f"Столбчатая диаграмма {column_name + ' 2'}")
+    ax4.set_title(f"Столбчатая диаграмма {column_1 + ' после изменения'}")
+    ax.tick_params(labelsize="small", labelrotation=330)
+    ax2.tick_params(labelrotation=330)
+    ax4.tick_params(labelrotation=330)
     plt.setp([ax2], xlabel='значения выборки', ylabel='частота')
-    plt.show()
-
-
-def compare_numeric_graph(data_1, data_2, column_1, column_2, column_name):
-    """
-    Сопоставляет графики двух числовых признаков
-    :param data_1: первый фрейм данных
-    :param data_2: второй фрейм данных
-    :param column_1: столбец первого фрейма
-    :param column_2: столбец второго фрейма
-    :param column_name: название сравниваемого параметра
-    """
-    num_1 = data_1[column_1].dropna()
-    num_2 = data_2[column_2]
-    fig = plt.figure(figsize=(17, 10))
-    ax = fig.add_subplot(231)
-    ax2 = fig.add_subplot(233)
-    ax3 = fig.add_subplot(232)
-    ax4 = fig.add_subplot(234)
-    ax5 = fig.add_subplot(236)
-    ax6 = fig.add_subplot(235)
-    ax.hist(num_1, bins=100)
-    ax.set_title(f"Гистограмма {column_name} числового параметра")
-    ax2.boxplot(x=num_1, data=data_1)
-    ax3.set_title(f"Оценка функции плотности {column_name} "
-                  f"числового параметра")
-    ax2.set_title(f"Диаграмма 'ящик с усами' {column_name} "
-                  f"числового параметра")
-    sns.kdeplot(num_1)
-    ax4.hist(num_2, bins=100)
-    ax4.set_title(f"Гистограмма {column_name} числового параметра")
-    ax5.boxplot(x=num_2, data=data_2)
-    ax6.set_title(f"Оценка функции плотности {column_name} "
-                  f"числового параметра")
-    ax5.set_title(f"Диаграмма 'ящик с усами' {column_name} "
-                  f"числового параметра")
-    sns.kdeplot(num_2)
-    plt.setp([ax, ax3, ax4, ax6], xlabel='значения выборки')
-    plt.setp([ax2, ax5], xlabel='номер выборки', ylabel='разброс значений')
-    plt.setp([ax, ax4], ylabel='частота')
-    plt.setp([ax3, ax6], ylabel='вероятность')
     plt.show()
